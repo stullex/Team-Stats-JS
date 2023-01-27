28046f2ac7fd17b3119c7ec344eccb7ef715694b
stullex <120705595+stullex@users.noreply.github.com>
2023-01-27 04:52:05 +0100

    Add files via upload

---
+++ teamstats.js
@@ -0,0 +1,47 @@
+const team = {
+    _player: [
+            { firstName: "Naruto", lastName: "Uzumaki", age: 16 },
+            { firstName: "Kakashi", lastName: "Hatake", age: 30 },
+            { firstName: "Sakura", lastName: "Haruno", age: 16 }
+    ],
+    _games: [
+            { opponent: "Kisame Hoshigaki", teamPoints: 69, opponentPoints: 31},
+            { opponent: "Danso Shimura", teamPoints: 78, opponentPoints: 22},
+            { opponent: "Nagato Uzumaki", teamPoints: 51, opponentPoints: 49}
+    ],
+
+    get players() {
+        return this._player
+    },
+    get games() {
+        return this._games
+    },
+
+
+    addPlayer(newFirstName, newLastName, newAge) {
+        let player = {
+            firstName: newFirstName,
+            lastName:newLastName,
+            age: newAge
+        }
+        this.players.push(player)
+    }, 
+
+    addGame(newOpponent, newTeamPoints, newOpponentPoints) {
+        let game = {
+            opponent: newOpponent, 
+            teamPoints: newTeamPoints,
+            opponentPoints: newOpponentPoints
+        }
+        this.games.push(game)
+    }
+
+}
+
+team.addGame("Gol D. Roger", 0, 1000)
+
+team.addPlayer("Sasuke", "Uchiha", 16)
+
+
+
+console.log(team.games)
